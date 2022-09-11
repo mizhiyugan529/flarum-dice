69701f7b902d829aab62ef1dd2cfb9aedecc94d4
L <xlhu170@icloud.com>
2022-09-11 22:22:01 +0800

    flarum-dices

---
+++ extend.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace ClarkWinkelmann\RollDie;
+
+use Flarum\Api\Serializer\PostSerializer;
+use Flarum\Extend;
+use Flarum\Post\Event\Saving;
+use Flarum\Post\Post;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Support\Arr;
+
+return [
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__ . '/js/dist/admin.js'),
+
+    (new Extend\Frontend('forum'))
+        ->css(__DIR__ . '/less/forum.less')
+        ->js(__DIR__ . '/js/dist/forum.js'),
+
+    new Extend\Locales(__DIR__ . '/locale'),
+
+    (new Extend\Event())
+        ->listen(Saving::class, function (Saving $event) {
+            $attributes = Arr::get($event->data, 'attributes', []);
+//            print_r($event->post);
+
+            if (!Arr::exists($attributes, 'content')) {
+                return;
+            }
+
+            /**
+             * @var $settings SettingsRepositoryInterface
+             */
+            $settings = resolve(SettingsRepositoryInterface::class);
+
+            $rolls = [];
+
+//            if ($event->post->dice_rolls_20 ) {
+//                $rolls = explode(" ", ($event->post->dice_rolls));
+//            }
+
+            // We don't actually care about permission to edit post content
+            // If the user isn't allowed or the content is invalid, the save will fail anyway
+            // and the values generated here will never be persisted to the database
+            // Negative lookahead is necessary to match multiple emojis immediately following each other
+            // Allow die in block quotes, because it's just too difficult to exclude this in the HTML parsing in the frontend
+            $pattern = '~(?<=\[dice])(?P<firstDice>\d?d\d+|\d+)(?P<addOne>\+|\-)?(?P<secDice>\d?d\d+|\d+)?(?=\[/dice])~i';
+            preg_match_all('~(?<=\[dice])(?P<firstDice>\d?d\d+|\d+)((?P<ifAddOne>\+|-)(?P<secondDice>\d?d\d+|\d+))?((?P<ifAddTwo>\+|-)(?P<thirdDice>\d?d\d+|\d+))?(?=\[/dice])~i', Arr::get($attributes, 'content'), $matches);
+
+            $numberOfRolls = count($matches[0]);
+
+            // We only generate additional numbers
+            // Even if some of the dice have been edited out, we keep the value in case they are added back in
+//            $rolls_dice_text = [];
+//            for ($i = count($rolls); $i < $numberOfRolls; $i++) {
+//                $roll_dice_text = $matches[0][$i];
+//                $dice1 = $matches["firstDice"][$i];
+//                $rolls_dice_text[] = $matches[0][$i];
+//            }
+            $seednum = 0;
+            try {
+                $seednum = $event->post->created_at->timestamp;
+            } catch (Throwable $e) {
+                $seednum = random_int(1, 9999999999);
+            }
+            $seednum = 1000 * $seednum;
+            try {
+                for ($i = count($rolls); $i < $numberOfRolls; $i++) {
+                    $rolls_dice_text = [];
+                    if ($matches["firstDice"][$i] != null) {
+                        $rolls_dice_text[] = $matches["firstDice"][$i];
+                    }
+                    if ($matches["secondDice"][$i] != null) {
+                        $rolls_dice_text[] = $matches["secondDice"][$i];
+                    }
+                    if ($matches["thirdDice"][$i] != null) {
+                        $rolls_dice_text[] = $matches["thirdDice"][$i];
+                    }
+                    $rolls_dice_num = 0;
+                    if ($matches["secondDice"][$i] != null) {
+                        $rolls_dice_result = "" . $matches[0][$i] . "=";
+                    } else {
+                        $rolls_dice_result = "" . $matches[0][$i];
+                    }
+                    $result_final = 0;
+                    foreach ($rolls_dice_text as $key => $value) {
+                        $rolls_dice_num = $rolls_dice_num + 1;
+                        //开始对每个进行处理
+                        $result = 0;
+                        if ($rolls_dice_num == 1) {
+                            $result_final = $result;
+                        } elseif ($rolls_dice_num == 2) {
+                            if ($matches["ifAddOne"][$i] != null) {
+                                if ($matches["ifAddOne"][$i] == "-") {
+                                    $rolls_dice_result = $rolls_dice_result . "-";
+                                } else {
+                                    $rolls_dice_result = $rolls_dice_result . "+";
+                                }
+                            }
+                        } elseif ($rolls_dice_num == 3) {
+                            if ($matches["ifAddTwo"][$i] != null) {
+                                if ($matches["ifAddTwo"][$i] == "-") {
+                                    $rolls_dice_result = $rolls_dice_result . "-";
+                                } else {
+                                    $rolls_dice_result = $rolls_dice_result . "+";
+                                }
+                            }
+                        }
+                        if ($value != null) {
+                            if (is_numeric($value)) {
+                                if ($matches["ifAddOne"][$i] != null) {
+                                    $rolls_dice_result = $rolls_dice_result . $value;
+                                }
+                                $result = $value;
+                            } else {
+                                $regex2 = '~(?P<d1>\d?)d(?P<d2>\d+)~i';
+                                preg_match($regex2, $value, $matches2);
+                                if ($matches2["d1"] == null) {
+                                    $matches2["d1"] = 1;
+                                }
+                                for ($add = 0; $add < $matches2["d1"]; $add++) {
+                                    srand($seednum+$i*100+$rolls_dice_num*10+$add);
+                                    $result_temp = rand(1, $matches2["d2"]);
+                                    if ($matches2["d1"] > 1) {
+                                        if ($add == 0) {
+                                            if ($matches["secondDice"][$i] == null) {
+                                                $rolls_dice_result = "" . $matches[0][$i]. "=";
+                                            }
+                                            $rolls_dice_result = $rolls_dice_result . "(" . (string)$result_temp;
+                                        } else {
+                                            $rolls_dice_result = $rolls_dice_result . "+" . (string)$result_temp;
+                                        }
+                                    } else {
+                                        if ($matches["ifAddOne"][$i] != null) {
+                                            $rolls_dice_result = $rolls_dice_result . "(" . $result_temp . ")";
+                                        }
+                                    }
+                                    $result += $result_temp;
+                                }
+                                if ($matches2["d1"] > 1) {
+                                    $rolls_dice_result = $rolls_dice_result . ")";
+                                }
+                            }
+                            if ($rolls_dice_num == 1) {
+                                $result_final = $result;
+                            } elseif ($rolls_dice_num == 2) {
+                                if ($matches["ifAddOne"][$i] != null) {
+                                    if ($matches["ifAddOne"][$i] == "-") {
+                                        $result_final -= $result;
+                                    } else {
+                                        $result_final += $result;
+                                    }
+                                }
+                            } elseif ($rolls_dice_num == 3) {
+                                if ($matches["ifAddTwo"][$i] != null) {
+                                    if ($matches["ifAddTwo"][$i] == "-") {
+                                        $result_final -= $result;
+                                    } else {
+                                        $result_final += $result;
+                                    }
+                                }
+                            }
+
+                        }
+
+
+                    }
+//                print_r($rolls_dice_result . "=" . (string)$result_final);
+                    $rolls[] = $rolls_dice_result . "=" . (string)$result_final;
+                }
+            } catch (Throwable $e) {
+                $rolls = [];
+            }
+
+
+            // The rolls are saved as characters in a single string
+            $event->post->dice_rolls_20 = implode(' ', $rolls);
+//            $event->post->dice_rolls = "3";
+//            $event->post->dice_rolls = $rolls[0];
+
+        }),
+
+    (new Extend\ApiSerializer(PostSerializer::class))
+        ->attributes(function (PostSerializer $serializer, Post $post): array {
+            if ($post->dice_rolls_20) {
+                return [
+                    'diceRolls20' => $post->dice_rolls_20,
+                ];
+            }
+
+            return [];
+        }),
+];
---
+++ js/src/forum/index.ts
@@ -0,0 +1,103 @@
+import {VnodeDOM} from 'mithril';
+import {extend, override} from 'flarum/common/extend';
+import app from 'flarum/forum/app';
+import Post from 'flarum/common/models/Post';
+import extractText from 'flarum/common/utils/extractText';
+import ItemList from 'flarum/common/utils/ItemList';
+import CommentPost from 'flarum/forum/components/CommentPost';
+import TextEditor from 'flarum/common/components/TextEditor';
+import TextEditorButton from 'flarum/common/components/TextEditorButton';
+
+
+function configureTooltip(element: Element) {
+  $(element).tooltip({
+    container: document.body,
+    placement: 'right',
+  });
+}
+
+app.initializers.add('roll-die-20', () => {
+  override(Post.prototype, 'contentHtml', function (this: Post, original: () => string) {
+    const contentHtml = original();
+    const rollsAsString = this.attribute('diceRolls20');
+
+    if (!rollsAsString) {
+      return contentHtml;
+    }
+
+    const rolls = rollsAsString.split(' ');
+    let index = 0;
+
+    // We will match every die emoji alone on its line
+    // A line might be wrapped in a paragraph, or have newlines before or after
+    // When the <br> is before, there's an additional newline in the HTML in between
+    // The first emoji needs to be outside of the [] rule because it's multi-byte
+    // Positive lookahead is necessary otherwise a series of emojis separated only by newlines will only match 1/2
+    var regObj = new RegExp("\\[dice](\\d?d\\d+|\\d+)((\\+|-)(\\d?d\\d+|\\d+))?((\\+|-)(\\d?d\\d+|\\d+))?\\[/dice]","gmi");
+    try{
+    return contentHtml.replace(regObj, (match, before) => {
+      const number = rolls[index++];
+      console.log(before);
+      const span = document.createElement('span');
+      span.className = 'roll-a-die-20';
+      span.dataset.number = number + '';
+      span.textContent = number;
+
+      return span.outerHTML;
+    });}catch (e) {
+      return contentHtml
+    }
+  });
+
+  extend(CommentPost.prototype, ['oncreate', 'onupdate'], function (returnValue: any, vnode: VnodeDOM) {
+    vnode.dom.querySelectorAll('.roll-a-die-20').forEach(element => {
+      configureTooltip(element);
+    });
+  });
+
+  // @ts-ignore global variables and many untyped parameters
+  override(s9e.TextFormatter, 'preview', (original, text, element) => {
+    original(text, element);
+
+    let walk;
+    let node;
+
+    // Clean up existing DOM to remove any now invalid span
+    walk = document.createTreeWalker(element);
+    while (node = walk.nextNode()) {
+      if (node instanceof HTMLElement && node.classList.contains('roll-a-die-20')) {
+        const text = document.createTextNode(node.textContent || '');
+        node.parentNode!.replaceChild(text, node);
+      }
+    }
+
+    // Convert valid text nodes into the preview span
+    walk = document.createTreeWalker(element, NodeFilter.SHOW_TEXT);
+    const replaceQueue = [];
+    while (node = walk.nextNode()) {
+      // Skip if no text content to please typescript
+      if (!node.textContent) {
+        continue;
+      }
+
+      // Skip if parent is already the result of a span conversion
+      if ((node.parentNode as HTMLElement).classList.contains('roll-a-die-20')) {
+        continue;
+      }
+
+    }
+
+  });
+
+  extend(TextEditor.prototype, 'toolbarItems', function (this: TextEditor, items: ItemList) {
+    items.add('roll-die-20', m(TextEditorButton, {
+      icon: 'fas fa-dice',
+      onclick: () => {
+        this.attrs.composer.editor.insertAtCursor('[dice][/dice]');
+        const range = this.attrs.composer.editor.getSelectionRange();
+        this.attrs.composer.editor.moveCursorTo(range[1] - 7);
+      },
+    }, app.translator.trans('ann-dice.forum.tooltip.preview')));
+  },999);
+}, 999);
+// Priority must be lower than Flarum's flarum-emoji, which has default priority, so any value could work
